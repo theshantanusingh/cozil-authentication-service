e9ccf2154056bc1f08390b5be122242f65ee2a3a
Shantanu Singh <distributedservices.shan@gmail.com>
2025-07-25 17:57:09 +0530

    feat(logout): created logout controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -36,11 +36,11 @@ async function loginController(req, res) {
 
         const exp = new Date(Date.now() + config.jwt.refreshTokenExpiry);
 
-        logger.info(`{module: auth.controller.js}, ${req.body.username} has setted up tokens, now saving the refreshtoken to db.`)
+        logger.info(`{module: auth.controller.js}, ${req.body.username} has setted up tokens, now saving the refreshtoken to db.`);
 
         await saveRefreshToken(user._id, refreshToken, exp);
 
-        logger.info(`{module: auth.controller.js}, ${req.body.username} has saved the refreshtoken to db.`)
+        logger.info(`{module: auth.controller.js}, ${req.body.username} has saved the refreshtoken to db.`);
 
         return res.status(200).json({
             "message": "Login successfull",
@@ -53,5 +53,31 @@ async function loginController(req, res) {
         logger.error(` { module: auth.contoller.js and error is ${err} } there is a error in logging the user with username ${req.body.username}`);
         return res.status(500).json({ message: "Internal Server Error" });
     }
-}
+};
 
+async function logoutContoller(req, res) {
+    const { refreshToken } = req.body;
+
+    try {
+
+        const deleted = await Token.findOneAndDelete({ token: refreshToken });
+
+        if (!deleted) {
+            logger.warn(`{module: auth.controller.js}, Refresh Token not found, maybe already logout.`)
+            res.status(200).json({
+                message: "Logged out (token may have already expired)"
+            })
+        }
+
+        logger.info(`{module: auth.controller.js} , User loggedout seccessfully, token removed from DB`);
+        res.status(200).json({
+            message: "User logged out successfully"
+        });
+
+    } catch (err) {
+        logger.error(`{module: auth.controller.js} ,Error during logout,  err: ${err}`)
+        res.status(500).json({
+            message: "Error logging out"
+        })
+    }
+};
